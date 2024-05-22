aed8df0f685011cdff246af3ab65d7d318ad4843
Vitor Freitas Ribeiro <programador.vitor.ribeiro@hotmail.com>
2024-05-22 00:25:21 -0300

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Product from "./components/Product.jsx";
+import CartContextProvider from "./store/ContextProvider.jsx";
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+
+function App() {
+  return (
+    <CartContextProvider>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </CartContextProvider>
+  );
+}
+
+export default App;
